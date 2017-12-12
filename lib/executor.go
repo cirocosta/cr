6c607609c1a5e4a49ba84d045f0a419d871e8aa6
Ciro S. Costa <ciro.costa@liferay.com>
2017-12-11 22:51:35 -0200

    Adds run formatting

--- lib/executor.go
+++ lib/executor.go
@@ -15,9 +15,10 @@ import (
 // Executor encapsulates the execution
 // context of a graph of jobs.
 type Executor struct {
-	config *Config
-	graph  *dag.AcyclicGraph
-	logger zerolog.Logger
+	config  *Config
+	graph   *dag.AcyclicGraph
+	logger  zerolog.Logger
+	jobsMap map[string]*Job
 }
 
 // New instantiates a new Executor from
@@ -37,10 +38,15 @@ func New(cfg *Config) (e Executor, err error) {
 
 	e.config = cfg
 	e.graph = &graph
+	e.jobsMap = map[string]*Job{}
 	e.logger = zerolog.New(os.Stdout).
 		With().
 		Str("from", "executor").
 		Logger()
+
+	for _, job := range cfg.Jobs {
+		e.jobsMap[job.Id] = job
+	}
 
 	return
 }
@@ -55,7 +61,7 @@ func (e *Executor) GetDotGraph() (res string) {
 // Execute initiates the parallel execution of the
 // jobs.
 func (e *Executor) Execute(ctx context.Context) (err error) {
-	err = TraverseAndExecute(ctx, e.graph)
+	err = e.TraverseAndExecute(ctx, e.graph)
 	if err != nil {
 		err = errors.Wrapf(err, "jobs execution failed")
 		return
@@ -98,12 +104,15 @@ func TemplateField(field string, state *RenderState) (res string, err error) {
 	return
 }
 
-func Execute(ctx context.Context, j *Job) (err error) {
+// RunJob is a method invoked for each vertex
+// in the execution graph except the root.
+func (e *Executor) RunJob(ctx context.Context, j *Job) (err error) {
 	var (
 		execution *Execution
 		output    bytes.Buffer
 		stdout    io.Writer = os.Stdout
 		stderr    io.Writer = os.Stderr
+		run       string
 	)
 
 	if j.CaptureOutput {
@@ -111,11 +120,20 @@ func Execute(ctx context.Context, j *Job) (err error) {
 		stderr = io.MultiWriter(&output, os.Stderr)
 	}
 
+	run, err = TemplateField(j.Run, &RenderState{
+		Jobs: e.jobsMap,
+	})
+	if err != nil {
+		err = errors.Wrapf(err,
+			"couldn't render run command")
+		return
+	}
+
 	execution = &Execution{
 		Argv: []string{
 			"/bin/bash",
 			"-c",
-			j.Run,
+			run,
 		},
 		Stdout: stdout,
 		Stderr: stderr,
@@ -132,7 +150,7 @@ func Execute(ctx context.Context, j *Job) (err error) {
 	return
 }
 
-func CreateExecutor(ctx context.Context) dag.WalkFunc {
+func (e *Executor) CreateWalkFunc(ctx context.Context) dag.WalkFunc {
 	return func(v dag.Vertex) error {
 		job, ok := v.(*Job)
 		if !ok {
@@ -143,15 +161,15 @@ func CreateExecutor(ctx context.Context) dag.WalkFunc {
 			return nil
 		}
 
-		return Execute(ctx, job)
+		return e.RunJob(ctx, job)
 	}
 }
 
 // TraverseAndExecute goes through the graph
 // provided and starts the execution of the jobs.
-func TraverseAndExecute(ctx context.Context, g *dag.AcyclicGraph) (err error) {
+func (e *Executor) TraverseAndExecute(ctx context.Context, g *dag.AcyclicGraph) (err error) {
 	w := &dag.Walker{
-		Callback: CreateExecutor(ctx),
+		Callback: e.CreateWalkFunc(ctx),
 	}
 
 	w.Update(g)
